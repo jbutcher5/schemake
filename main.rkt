b9c26db46b10972be5ba475845c41477eaf2d2cf
James Butcher <jamesbutcher167@gmail.com>
2023-05-29 01:18:59 +0100

    Refactor entire project

--- main.rkt
+++ main.rkt
@@ -1,10 +1,32 @@
 #lang racket
 
 (define ninja-output "")
+(define ninja-status 0)
 
-(define (ninja-append! str)
-  (set! ninja-output (string-append ninja-output "\n" str "\n")))
+(define-syntax-rule (append-ln! buf str)
+  (set! buf (string-append buf "\n" str)))
 
-(ninja-append! "Hello, World!")
+(define-syntax-rule (step! a ...)
+  (when (ninja-status . = . 0)
+    (define x (a ...))
 
-(display ninja-output)
+    (if (null? x)
+        (set! ninja-status 1)
+        (append-ln! ninja-output x))))
+
+(define (rule name vars)
+  (cond
+    [(hash-has-key? vars 'command)
+     (begin
+       (define buffer "")
+       (append-ln! buffer (format "rule ~a" name))
+       (for ([(key val) vars])
+         (append-ln! buffer (format "  ~a = ~a" key val)))
+       buffer)]
+    [else null]))
+
+(define (main)
+  (step! rule 'foo (hash 'foo "bar" 'command "cowsay"))
+  (display ninja-output))
+
+(main)
