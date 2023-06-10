00082260a7b35f7f9b489afb6b9467c8a81e7bc9
James Butcher <jamesbutcher167@gmail.com>
2023-06-10 19:39:28 +0000

    Add project! function

--- main.rkt
+++ main.rkt
@@ -2,12 +2,20 @@
 (require file/glob)
 
 (define VERSION 0.0)
+(define PROJECT null)
+(define LANGUAGE null)
 
 (define (require-minimum-version ver)
   (when (VERSION . < . ver)
     (raise (format "The current version is too low try a version >=~a" ver) #t)))
 
+(define (project! name lang)
+  (set! PROJECT name)
+  (set! LANGUAGE lang))
+
 (define (main)
-  (require-minimum-version 0.1))
+  (require-minimum-version 0.1)
+  (project! "Schemake Test" )
+  )
 
 (main)
