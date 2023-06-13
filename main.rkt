13e6b410cccf8d917a0ea3cef8e876a7bbafeae2
James Butcher <jamesbutcher167@gmail.com>
2023-06-13 16:31:50 +0000

    Use templates for creating a new language target

--- main.rkt
+++ main.rkt
@@ -23,34 +23,42 @@
   (when (VERSION . < . ver)
     (raise (format "The current version is too low try a version >=~a" ver) #t)))
 
+(define (c-template! name src b-rules b-builds)
+  (define rules (unbox b-rules))
+  (define builds (unbox b-builds))
+  (set-add! rules (rule 'cc "gcc -c $in -o $out"))
+
+  (define obj '())
+  (for ((f src))
+    (define out (regexp-replace #rx"\\.c" f ".o"))
+    (set! obj (cons out obj))
+    (set-add! builds (build out 'cc f)))
+
+  (define obj-str (let obj->str ([o obj])
+                    (match o
+                      [`(,x) x]
+                      ['() ""]
+                      [xs (format "~a ~a" (car xs) (obj->str (cdr xs)))])))
+
+  (set-add! rules (rule 'link "gcc $in -o $out"))
+  (set-add! builds (build name 'link obj-str))
+  (set-box! b-rules rules)
+  (set-box! b-builds builds))
+
 (define (project! name lang)
   (set! PROJECT name)
   (set! LANGUAGE lang))
 
-(define (new-target! name src (lang null))
+(define (new-target! name src #:lang (lang null) #:template (t! null))
   (define language LANGUAGE)
   (when (not (null? lang))
     (set! language lang))
 
-  (match language
-    ['c (begin
-          (set-add! RULES (rule 'cc "gcc -c $in -o $out"))
-
-          (define obj '())
-          (for ((f src))
-            (define out (regexp-replace #rx"\\.c" f ".o"))
-            (set! obj (cons out obj))
-            (set-add! BUILDS (build out 'cc f)))
-
-          (define obj-str (let obj->str ([o obj])
-                            (match o
-                              [`(,x) x]
-                              ['() ""]
-                              [xs (format "~a ~a" (car xs) (obj->str (cdr xs)))])))
-
-          (set-add! RULES (rule 'link "gcc $in -o $out"))
-          (set-add! BUILDS (build name 'link obj-str)))]
-    ['() (raise "No language has been specified" #t)]))
+  (if (null? t!)
+      (match language
+        ['c (c-template! name src (box RULES) (box BUILDS))]
+        ['() (raise "No language has been specified" #t)])
+      (t! name src (box RULES) (box BUILDS))))
 
 (define (write-ninja! (file-name "build.ninja"))
   (define f (open-output-file file-name #:exists 'update))
